a062b2829756c10e2b177526f3a9e2e3f63b6e17
potnuriparimala24csm-ctrl <potnuriparimala.24.csm@anits.edu.in>
2026-09-17 14:29:03 +0530

    commit

---
+++ WEEK-3/script.js
@@ -0,0 +1,39 @@
+let students=[];
+// Function to add a student
+function addStudent() {
+    const input = document.getElementById("studentName");
+    const name = input.value.trim();
+
+    if (name === "") {
+        alert("Enter student name");
+        return;
+    }
+
+    students.push(name);
+    input.value = "";
+    displayStudents();
+}
+
+// Function to display students
+function displayStudents() {
+    const list = document.getElementById("studentList");
+    list.innerHTML = "";
+
+    students.forEach((student, index) => {
+        list.innerHTML += `
+            <li>
+                ${index + 1}. ${student}
+                <button class="delete" onclick="deleteStudent(${index})">Delete</button>
+            </li>
+        `;
+    });
+
+    document.getElementById("count").textContent = students.length;
+}
+
+// Function to delete a student
+function deleteStudent(index) {
+    students.splice(index, 1);
+    displayStudents();
+}
+
